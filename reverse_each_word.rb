266d9094ec01944b9222c933a91ac8a4707d3a0f
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-25 02:11:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,22 @@
-#phrase = "Hello there, and how are you?"
+def reverse_each_word_with_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
 
-def reverse_each_word(phrase)
+def reverse_each_word(string)
+  array = string.split(" ")
+  test_array = []
+  array.collect do|string|
+    test_array << string.reverse 
+  end
+  test_array.join(" ")
+end
 
-end 
+
+reverse_each_word("Hello there, and how are you?")
+
+  
